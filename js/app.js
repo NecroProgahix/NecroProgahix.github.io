a342035ff759b48dc2e3bc5188c7ccb73bde5865
NecroProgahix <artym.nosov1234@gmail.com>
2024-12-25 22:51:36 +0300

    updated a few pages and added new ones

--- js/app.js
+++ js/app.js
@@ -1,9 +1,18 @@
 // общие для всех страниц скрипты сюда
 
+let activeSession = false;
+
 function checkSession() {
     if (activeSession == false) {
         // добавить открытие login.html
         window.location.href = "login.html";
     }
     // иначе открыть account.html
+}
+
+function addToCart(productName, price) {
+    const cart = JSON.parse(localStorage.getItem('cart')) || [];
+    cart.push({ name: name, price });
+    localStorage.setItem('cart', JSON.stringify(cart));
+    alert(`${productName} добавлен в корзину.`);
 }

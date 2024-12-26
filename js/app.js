320d82632fff676adcf80bec8ac1e799cd640533
NecroProgahix <artym.nosov1234@gmail.com>
2024-12-26 06:41:24 +0300

    switched from using Web Content to indexedDB

--- js/app.js
+++ js/app.js
@@ -2,9 +2,10 @@
 
 let activeSession = false;
 
-function checkSession() {
+function checkSession(callback) {
     if (activeSession == false) {
         // добавить открытие login.html
+        console.log(callback);
         window.location.href = "login.html";
         if (callback == true) { return true ; } //
     }
@@ -19,4 +20,52 @@ function addToCart(productName, price) {
         localStorage.setItem('cart', JSON.stringify(cart));
         alert(`${productName} добавлен в корзину.`);
     }
+}
+
+const dbName = 'ProductsDB'; // тяжёлая игрушка
+const dbVersion = 1;
+
+function initDB() {
+    return new Promise((resolve, reject) => {
+        const request = indexedDB.open(dbName, dbVersion);
+
+        request.onupgradeneeded = (event) => {
+            const db = event.target.result;
+            if (!db.objectStoreNames.contains('products')) {
+                const store = db.createObjectStore('products', { keyPath: 'id', autoIncrement: true });
+                store.createIndex('name', 'name', { unique: false });
+                store.createIndex('price', 'price', { unique: false });
+                store.createIndex('image', 'image', { unique: false });
+            }
+        };
+
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    });
+}
+
+// для первоначального добавления
+async function addProducts(db) {
+    const products = [
+        { name: "Iphone 14", price: "$599", image: "img/200px-IPhone_14.webp" },
+        { name: "MacBook Air", price: "$1,199", image: "img/300px-MacBook_Air_(15-inch,_M3,_2024).webp" },
+        { name: "Ipad Air 4", price: "$599", image: "img/300px-IPad_Air_(5th_generation).webp" },
+        { name: "AirPods Pro 2", price: "$249", image: "img/300px-AirPods_Pro_(2nd_generation).webp" },
+        { name: "XPS 13", price: "$999", image: "img/notebook-xps-13-9345-t-gray-gallery-2.avif" }
+    ];
+
+    const transaction = db.transaction('products', 'readwrite');
+    const store = transaction.objectStore('products');
+
+    const existingProducts = await new Promise((resolve, reject) => {
+        const request = store.getAll();
+        request.onsuccess = () => resolve(request.result);
+        request.onerror = () => reject(request.error);
+    });
+
+    products.forEach(product => {
+        if (!existingProducts.some(existing => existing.name === product.name)) {
+            store.add(product);
+        }
+    });
 }
